1f8836bd45e1e6d4d7331fe454be233d377432ad
Ariel Madisha <65048030+ArielMadisha@users.noreply.github.com>
2025-11-25 06:29:16 +0200

    Add files via upload

---
+++ contact/index.php
@@ -0,0 +1,11 @@
+<?php
+require '../top-section.php';
+require 'info.html';
+require '../bottom-section.php';
+?>
+<script>
+    $('#contact').addClass('active');
+    page = 'contact';
+</script>
+
+<?php require '../closing-section.php'; ?>
